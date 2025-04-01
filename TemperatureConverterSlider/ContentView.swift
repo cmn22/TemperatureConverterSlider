b84473a6b6fb78c60b7d0e0c7381b4ce6a2317b9
Chaitanya Malani <chaitanyamalani.rns@gmail.com>
2025-04-01 16:26:23 +0200

    Temperature Convertor

--- TemperatureConverterSlider/ContentView.swift
+++ TemperatureConverterSlider/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var temperature: Double = 0.0
+    
+    var celsius: Double {
+        return temperature
+    }
+
+    var fahrenheit: Double {
+        return (temperature * 9/5) + 32
+    }
+
+    var kelvin: Double {
+        return temperature + 273.15
+    }
+
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            HStack(spacing: 0) {
+                // Celsius Section
+                VStack{
+                    Text("Celsius")
+                        .bold()
+                    Text("\(celsius, specifier: "%.2f")")
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.red)
+                .foregroundColor(.black)
+                
+                // Fahrenheit Section
+                VStack{
+                    Text("Fahrenheit")
+                        .bold()
+                    Text("\(fahrenheit, specifier: "%.2f")")
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.blue)
+                .foregroundColor(.black)
+                
+                // Kelvin Section
+                VStack{
+                    Text("Kelvin")
+                        .bold()
+                    Text("\(kelvin, specifier: "%.2f")")
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.green)
+                .foregroundColor(.black)
+            }
+            .edgesIgnoringSafeArea(.all)
+            
+            // Temperature Slider
+            VStack{
+                Spacer()
+                Slider(value: $temperature, in: -50...100, step: 0.1)
+                    .padding()
+                    .tint(Color.yellow)
+            }
         }
-        .padding()
     }
 }
 
