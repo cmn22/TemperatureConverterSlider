a7718c9cbec9b026b48036d0a1ed4443df191e50
Chaitanya Malani <chaitanyamalani.rns@gmail.com>
2025-04-01 15:43:04 +0200

    Initial Commit

---
+++ TemperatureConverterSlider/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TemperatureConverterSlider
+//
+//  Created by Chaitanya Malani on 01/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TemperatureConverterSlider/TemperatureConverterSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterSliderApp.swift
+//  TemperatureConverterSlider
+//
+//  Created by Chaitanya Malani on 01/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
